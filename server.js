41a7d003ef3130ec6494f889ae8e62c719a97b56
Andre Lademann <andre.lademann@netresearch.de>
2020-04-07 00:51:46 +0200

    Add a nighmare of screenshot tool

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger')
 
+const Nightmare = require('nightmare')
+const screenshotSelector = require('nightmare-screenshot-selector')
+const fs = require('fs')
+
 const config = {
   name: 'boxshot-service',
   port: 80,
@@ -17,7 +21,16 @@ app.use(cors())
 app.use(ExpressAPILogMiddleware(logger, { request: true }))
 
 app.get('/', (req, res) => {
-  res.status(200).send('hello world ...')
+  Nightmare.action('screenshotSelector', screenshotSelector)
+
+  var nightmare = Nightmare()
+  nightmare
+    .goto('https://www.netresearch.de/')
+    .screenshotSelector('h1') // get the image in a buffer
+    .then(function (data) {
+      fs.writeFileSync('shots/screen.png', data)
+      res.status(200).send('Screenshot has been created.')
+    })
 })
 
 app.listen(config.port, config.host, (e) => {
