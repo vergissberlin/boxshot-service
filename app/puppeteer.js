9852d7ecb2ca441d3b7cc438e5bd82bdaac31a53
Andre Lademann <andre.lademann@netresearch.de>
2020-04-15 00:35:46 +0200

    respect style hints

--- app/puppeteer.js
+++ app/puppeteer.js
@@ -1,20 +1,20 @@
 const puppeteer = require('puppeteer')
 
 ;(async () => {
-  const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
+	const browser = await puppeteer.launch({
+		args: [
+			'--no-sandbox',
+			'--disable-setuid-sandbox',
 
-      // debug logging
-      '--enable-logging',
-      '--v=1',
-    ],
-  })
+			// debug logging
+			'--enable-logging',
+			'--v=1',
+		],
+	})
 
-  const page = await browser.newPage()
+	const page = await browser.newPage()
 
-  await page.goto('https://www.google.com/', { waitUntil: 'networkidle2' })
+	await page.goto('https://www.google.com/', { waitUntil: 'networkidle2' })
 
-  browser.close()
+	browser.close()
 })()
--- app/server.js
+++ app/server.js
@@ -2,12 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger')
-const fs = require('fs')
 
 const config = {
-  name: 'boxshot-service',
-  port: process.env.PORT || 80,
-  host: '0.0.0.0',
+	name: 'boxshot-service',
+	port: process.env.PORT || 80,
+	host: '0.0.0.0',
 }
 
 const app = express()
@@ -18,12 +17,12 @@ app.use(cors())
 app.use(ExpressAPILogMiddleware(logger, { request: true }))
 
 app.get('/', (req, res) => {
-  res.json({ hello: 'world' })
+	res.json({ hello: 'world' })
 })
 
 app.listen(config.port, config.host, (e) => {
-  if (e) {
-    throw new Error('Internal Server Error')
-  }
-  logger.info(`${config.name} running on ${config.host}:${config.port}`)
+	if (e) {
+		throw new Error('Internal Server Error')
+	}
+	logger.info(`${config.name} running on ${config.host}:${config.port}`)
 })
