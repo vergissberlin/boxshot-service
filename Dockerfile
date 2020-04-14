b413fe5094e8d0f5d8fa9098bab905ef55ad9cee
Andre Lademann <andre.lademann@netresearch.de>
2020-04-15 00:16:33 +0200

    Add herko deployment scripts

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,15 @@
-FROM node:12
+FROM alekzonder/puppeteer
 
 # Create app directory
-WORKDIR /usr/src/app
+WORKDIR /app
 
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
-COPY package*.json ./
+# Copy application files
+COPY app /app
 
+# Install dependencies
 RUN npm install
 # If you are building your code for production
-# RUN npm ci --only=production
-
-# Bundle app source
-COPY . .
+#RUN npm ci --only=production
 
 EXPOSE 80
 CMD ["npm", "run", "start"]
--- app/server.js
+++ app/server.js
@@ -2,14 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger')
-
-const Nightmare = require('nightmare')
-const screenshotSelector = require('nightmare-screenshot-selector')
 const fs = require('fs')
 
 const config = {
   name: 'boxshot-service',
-  port: 80,
+  port: process.env.PORT || 80,
   host: '0.0.0.0',
 }
 
@@ -21,16 +18,7 @@ app.use(cors())
 app.use(ExpressAPILogMiddleware(logger, { request: true }))
 
 app.get('/', (req, res) => {
-  Nightmare.action('screenshotSelector', screenshotSelector)
-
-  var nightmare = Nightmare()
-  nightmare
-    .goto('https://www.netresearch.de/')
-    .screenshotSelector('h1') // get the image in a buffer
-    .then(function (data) {
-      fs.writeFileSync('shots/screen.png', data)
-      res.status(200).send('Screenshot has been created.')
-    })
+  res.json(req)
 })
 
 app.listen(config.port, config.host, (e) => {
