5a47d07ab2ae99a01689f91be677da2bad9cf708
Andre Lademann <andre.lademann@netresearch.de>
2020-04-15 00:59:07 +0200

    Respect style hints

--- app/server.js
+++ app/server.js
@@ -4,9 +4,9 @@ const cors = require('cors')
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger')
 
 const config = {
-  name: 'boxshot-service',
-  port: process.env.PORT || 80,
-  host: '0.0.0.0',
+	name: 'boxshot-service',
+	port: process.env.PORT || 80,
+	host: '0.0.0.0',
 }
 
 const app = express()
@@ -17,16 +17,16 @@ app.use(cors())
 app.use(ExpressAPILogMiddleware(logger, { request: true }))
 
 app.get('/', (req, res) => {
-  res.header(
-    'Cache-Control',
-    'max-age=600, s-max-age=600, public, must-revalidate'
-  )
-  res.json({ hello: 'world' })
+	res.header(
+		'Cache-Control',
+		'max-age=600, s-max-age=600, public, must-revalidate'
+	)
+	res.json({ hello: 'world' })
 })
 
 app.listen(config.port, config.host, (e) => {
-  if (e) {
-    throw new Error('Internal Server Error')
-  }
-  logger.info(`${config.name} running on ${config.host}:${config.port}`)
+	if (e) {
+		throw new Error('Internal Server Error')
+	}
+	logger.info(`${config.name} running on ${config.host}:${config.port}`)
 })
